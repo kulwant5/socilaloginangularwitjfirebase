8eba3ec3630ed7e628acfdc142f45e16054904de
kulwant5 <kulwant.singh@kabirinfo.com>
2019-06-05 17:24:08 +0530

    second project

---
+++ firebase-authentication-with-angular-7-master/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+   apiKey: "AIzaSyAMGXUfeYKEHlFLNtSVGh14pIoKzPR1n_8",
+  authDomain: "fir-project-611a7.firebaseapp.com",
+  databaseURL: "https://fir-project-611a7.firebaseio.com",
+  projectId: "fir-project-611a7",
+  storageBucket: "fir-project-611a7.appspot.com",
+  messagingSenderId: "117078562055",
+  appId: "1:117078562055:web:10a2a36b560a1272"
+  }
+};
